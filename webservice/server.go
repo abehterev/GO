8da87b77d723409db37ec38c40a09c4f6874937e
Andrey Bekhterev <info@behterev.su>
2013-11-21 18:18:34 +0400

    add own server instead http

--- webservice/server.go
+++ webservice/server.go
@@ -4,27 +4,42 @@ import (
 	"net/http"
 	"io"
 	"fmt"
+	"time"
 )
 
 func requestHandler(w http.ResponseWriter, r *http.Request) {
 	var (
 		out string
 	)
+
 	w.Header().Set("Content-Type", "text/plain")
 	w.Header().Set("Server", "WebService/0.1")
+
 	out += "Hello, world\n"
+	out += "Remote addr: " + r.RemoteAddr + "\n"
 	out += "You are use: " + r.Header.Get("User-Agent") + "\n"
 	out += "METHOD: " + r.Method + "\n"
 	out += "rawURI: " + r.RequestURI + "\n"
 	out += "URI: " + r.URL.Path + "\n"
+
 	io.WriteString(w, out)
 	fmt.Println(out)
 }
 
 func main(){
+
+	ser := &http.Server{
+		Addr:           ":8080",
+		Handler:        nil,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,	
+	}
+
 	http.HandleFunc("/", requestHandler)
-	err := http.ListenAndServeTLS(":8080", "test.crt", "test.key.nopass", nil)
+	err := ser.ListenAndServeTLS("test.crt","test.key.nopass")
+
 //	http.ListenAndServe(":8080", nil)
+
 	if err != nil {
 		fmt.Println("Err: " + err.Error() + "\n")
 	}
