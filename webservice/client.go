a8d8d2fa04982b63e9c3ac5b6f165f9b22b7a955
Andrey Bekhterev <info@behterev.su>
2013-11-22 21:00:27 +0400

    client mod

--- webservice/client.go
+++ webservice/client.go
@@ -4,30 +4,36 @@ import (
 	"net/http"
 	"net/url"
 	"crypto/tls"
+	"io/ioutil"
+	"log"
 	"fmt"
 )
+
+func failOnError(err error, msg string) {
+        if err != nil {
+                log.Fatalf("%s: %s", msg, err)
+                panic(fmt.Sprintf("%s: %s", msg, err))
+        }
+}
 
 func main(){
 
 	tr := &http.Transport{
-	//	TLSClientConfig:	&tls.Config{RootCAs: pool},
 		DisableCompression:	true,
 		TLSClientConfig:	&tls.Config{InsecureSkipVerify: true},
 	}
 
 	client := &http.Client{
-	//	CheckRedirect: redirectPolicyFunc,
 		Transport: tr,
 	}
 	
 	resp, err := client.PostForm("https://localhost:8080/", url.Values{"key": {"Value"}, "id": {"123"},})
+	failOnError(err, "Failed to connect")
+	
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	failOnError(err, "Failed to get body")
 
-	//resp, err := client.Get("http://localhost:8080/robots.txt")
-
-	resp.Body.Close()	
-
-	if err != nil {
-		fmt.Println("Err: " + err.Error() + "\n")
-	}
+	fmt.Sprintf("BODY:\n%s", body)
 
 }
