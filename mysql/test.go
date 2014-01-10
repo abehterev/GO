f67c4c53dd0e282eb5f183de680edbc10925ad37
Andrey Bekhterev <info@behterev.su>
2014-01-10 22:51:54 +0400

    Modify test.go for parsing config

--- mysql/test.go
+++ mysql/test.go
@@ -5,10 +5,35 @@ import(
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"fmt"
+	"encoding/json"
+	"os"
 )
 
 type Login interface {
 	GetByLogin(l string) (bool, error)
+}
+
+type TConfig struct {
+	Server string
+	Login string
+	Password string
+	Database string
+}
+
+func (c *TConfig) ReadConfig(file string) error {
+
+	f, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+
+	decoder := json.NewDecoder(f)
+	if decoder.Decode(&c) != nil {
+		return err
+	}
+
+	return nil
+	
 }
 
 type TAuth struct {
@@ -17,9 +42,9 @@ type TAuth struct {
     md5 string
 }
 
-func (a *TAuth) GetByLogin(l string) (bool, error){
+func (a *TAuth) GetByLogin(l string, C *TConfig) (bool, error){
 
-	db, err := sql.Open("mysql", "musicbox:musicboxpass@/musicbox")
+	db, err := sql.Open("mysql", C.Login+":"+C.Password+"@"+C.Server+"/"+C.Database)
 	if err != nil {
 		return false, err
 	}
@@ -31,11 +56,14 @@ func (a *TAuth) GetByLogin(l string) (bool, error){
 	}
 
 	rows, err := db.Query("SELECT id,md5pass FROM users WHERE login = ?", l)
-	if err != nil {
+	/*if err != nil {
         	return false, err
-	}
+	}*/
 
-	var c int = 0;
+	var c int = 0
+	if (err == sql.ErrNoRows){
+		fmt.Printf("Err: Nothing to het\n")
+	}
 	for rows.Next() {
 		c++;
 	        if err := rows.Scan(&a.id, &a.md5); err != nil {
@@ -59,7 +87,16 @@ func (a *TAuth) GetByLogin(l string) (bool, error){
 func main(){
 	Auth := new(TAuth)
 
-	r, err := Auth.GetByLogin("test123")
+	C := new(TConfig)
+	err := C.ReadConfig("conf.json")
+	if (err != nil) {
+		fmt.Printf("Config read error: %s\n",err.Error())
+		os.Exit(1)
+	}else{
+		fmt.Println(*C)
+	}
+
+	r, err := Auth.GetByLogin("test", C)
 	if (r){
 		fmt.Printf("We get data: [%d][%s][%s]\n", Auth.id, Auth.login, Auth.md5)
 	}else{
